94183a29f54d6226b05a6104934604f916be06e5
opencainiao <25662996@qq.com>
2015-08-27 13:03:14 +0800

    配置spring-mvc返回编码为utf-8

--- src/main/java/controller/WxCheckDevelopController.java
+++ src/main/java/controller/WxCheckDevelopController.java
@@ -1,17 +1,20 @@
 package controller;
 
+import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.mou.common.JsonUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mou01.core.domain.WxTextMessage;
 import com.mou01.core.util.CheckUtilWx;
 import com.mou01.core.util.HttpServletRequestUtil;
 import com.mou01.core.util.WxMessageUtil;
@@ -73,26 +76,36 @@ public class WxCheckDevelopController {
 		logger.debug(params);
 		try {
 
-			String signature = HttpServletRequestUtil.getTrimParameter(request,
-					"signature");
-			String timestamp = HttpServletRequestUtil.getTrimParameter(request,
-					"timestamp");
-			String nonce = HttpServletRequestUtil.getTrimParameter(request,
-					"nonce");
-			String echostr = HttpServletRequestUtil.getTrimParameter(request,
-					"echostr");
-
 			Map<String, String> paramsMap = WxMessageUtil.xml2Map(request);
-			
-			// WxMessageUtil.textMessage2Xml(message);
-			if (CheckUtilWx.checkSignature(signature, timestamp, nonce)) {
-				return echostr;
+			System.out.println(JsonUtil.getPrettyJsonStr(paramsMap));
+
+			String ToUserName = paramsMap.get("ToUserName");// 开发者微信号
+			String FromUserName = paramsMap.get("FromUserName");// 发送方帐号（一个OpenID）
+			String CreateTime = paramsMap.get("CreateTime");// 消息创建时间 （整型）
+			String MsgType = paramsMap.get("MsgType");// 开发者微信号
+			String Content = paramsMap.get("Content");// 文本消息内容
+			String MsgId = paramsMap.get("MsgId");// 消息id，64位整型
+
+			String message = null;
+			if ("text".equals(MsgType)) {
+				WxTextMessage wxTextMessage = new WxTextMessage();
+
+				wxTextMessage.setFromUserName(ToUserName);
+				wxTextMessage.setToUserName(FromUserName);
+				wxTextMessage.setMsgType("text");
+				wxTextMessage.setCreateTime(new Date().getTime());
+				wxTextMessage.setContent("您发送的消息是：" + Content);
+
+				message = WxMessageUtil.textMessage2Xml(wxTextMessage);
+				
+				logger.debug("服务器返回的消息\n{}",message);
 			}
+
+			return message;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return false;
 	}
-
 }
